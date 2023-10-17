b2a10c2c36fe6cffc099ecbc37693ecfcce3d393
mmlengah <98457199+mmlengah@users.noreply.github.com>
2023-10-17 19:33:54 +0100

    Removed opengl rendering for now

--- RayTracer/RayTracer/main.cpp
+++ RayTracer/RayTracer/main.cpp
@@ -1,13 +1,11 @@
 #include <SDL.h>
-#include <GL/glew.h>
 
 class AppWindow {
 private:
     SDL_Window* window;
-    SDL_GLContext glContext;
 
 public:
-    AppWindow() : window(nullptr), glContext(nullptr) {}
+    AppWindow() : window(nullptr) {}
 
     bool initialize(const char* title, int width, int height) {
         if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -15,24 +13,8 @@ public:
             return false;
         }
 
-        window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_OPENGL);
+        window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
         if (!window) {
-            SDL_Quit();
-            return false;
-        }
-
-        glContext = SDL_GL_CreateContext(window);
-        if (!glContext) {
-            SDL_DestroyWindow(window);
-            SDL_Quit();
-            return false;
-        }
-
-        glewExperimental = GL_TRUE;
-        GLenum glewError = glewInit();
-        if (glewError != GLEW_OK) {
-            SDL_GL_DeleteContext(glContext);
-            SDL_DestroyWindow(window);
             SDL_Quit();
             return false;
         }
@@ -49,18 +31,13 @@ public:
                 if (event.type == SDL_QUIT) {
                     running = false;
                 }
-                // Handle other events as needed (e.g. keyboard, mouse)
             }
 
-            // Rendering logic here
-            SDL_GL_SwapWindow(window);
+            SDL_UpdateWindowSurface(window);
         }
     }
 
     ~AppWindow() {
-        if (glContext) {
-            SDL_GL_DeleteContext(glContext);
-        }
         if (window) {
             SDL_DestroyWindow(window);
         }
@@ -70,7 +47,7 @@ public:
 
 int main(int argc, char** argv) {
     AppWindow appWindow;
-    if (!appWindow.initialize("GPU Ray Tracer", 600, 400)) {
+    if (!appWindow.initialize("Simple SDL Window", 600, 400)) {
         return 1;
     }
 
